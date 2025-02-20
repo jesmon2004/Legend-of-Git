589a42009bf6595c399615a858ca0e57d0c408f1
Jesús Gómez Montañés <jegomezmon@gmail.com>
2025-02-20 20:05:55 +0100

    Añadimos cambios en el README y un documento de java para crear niveles

--- Legend_of_git/src/niveles/LevelDesign.java
+++ Legend_of_git/src/niveles/LevelDesign.java
@@ -1,9 +1,24 @@
 package niveles;
 
 public class LevelDesign {
+    private String levelName;
+    private int difficulty;
+    private String environment;
 
-	public LevelDesign() {
-		// TODO Auto-generated constructor stub
-	}
+    public LevelDesign(String levelName, int difficulty, String environment) {
+        this.levelName = levelName;
+        this.difficulty = difficulty;
+        this.environment = environment;
+    }
 
+    public void displayLevelInfo() {
+        System.out.println("Nivel: " + levelName);
+        System.out.println("Dificultad: " + difficulty);
+        System.out.println("Entorno: " + environment);
+    }
+
+    public static void main(String[] args) {
+        LevelDesign level1 = new LevelDesign("Commit Caverns", 3, "Subterráneo");
+        level1.displayLevelInfo();
+    }
 }
--- Legend_of_git/src/personajes/Hero.java
+++ Legend_of_git/src/personajes/Hero.java
@@ -3,16 +3,31 @@
  */
 package personajes;
 
-/**
- * 
- */
-public class Hero {
 
-	/**
-	 * 
-	 */
-	public Hero() {
-		// TODO Auto-generated constructor stub
-	}
+public class Hero {
+    private String name;
+    private int level;
+    private int power;
+
+    public Hero(String name) {
+        this.name = name;
+        this.level = 1;
+        this.power = 10;
+    }
+
+    public void levelUp() {
+        level++;
+        power += 5;
+        System.out.println(name + " ha subido al nivel " + level + "! Poder: " + power);
+    }
+
+    public void attack(Villain villain) {
+        System.out.println(name + " ataca a " + villain.getName() + "!");
+        villain.takeDamage(power);
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
